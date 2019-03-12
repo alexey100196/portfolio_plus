3caa73464c3269a181aeecc7a42a258efde443e0
alexey100196 <alexeysobolev071@gmail.com>
2019-03-12 20:01:26 +0200

    01

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-1.8.1/slick/slick.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
